c03695eb33dde165076f6d1ccddf9bf0a5a9d519
Destiny Efe <destinyabusomwan@gmail.com>
2021-06-09 23:18:49 +0100

    Completed Home Page

---
+++ Shipper/static/js/index.js
@@ -0,0 +1,14 @@
+// //Adding and removing banner dark background
+// let nav = document.querySelector('nav');
+
+// window.addEventListener('scroll', function(){
+//     if(window.pageYOffset > 50){
+//         nav.classList.add('bg-dark', 'shadow')
+//     }else{
+//         nav.classList.remove('bg-dark', 'shadow')
+//     }
+// });
+
+
+
+
